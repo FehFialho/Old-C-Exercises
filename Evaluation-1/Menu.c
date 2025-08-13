81a62240d3def868032f2d388519451156ba6ae9
FehFialho <fernandaklechowicz@gmail.com>
2025-08-13 11:46:19 -0300

    Add Evaluation1 First Ex

---
+++ Evaluation-1/Menu.c
@@ -0,0 +1,56 @@
+// Evaluation 1 - (04/22/2025)
+// Menu-driven program that performs greetings, checks if a number is even or odd, and converts Celsius to Fahrenheit.
+#include <stdio.h>
+#include <string.h>
+
+void menu();
+
+int main(){
+
+    int choice = 1, num;
+    float fah, cel;
+    char name[50];
+
+    do {
+        menu();
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+                printf("\nEnter your name: ");
+                scanf("%s", name);
+                printf("Hello! Welcome %s!\n", name);
+            break;
+            case 2:
+                printf("\nEnter a number: ");
+                scanf("%d", &num);
+                if (num % 2 == 0){
+                    printf("The number %d is even!\n", num);    
+                } else {
+                    printf("The number %d is odd!\n", num);    
+                }
+            break;
+            case 3:
+                printf("\nEnter the value in Celsius: ");
+                scanf("%f", &cel);
+                fah = (cel * 9/5) + 32;
+                printf("%.2f °C --> %.2f °F\n", cel, fah);
+            break;
+            case 4:
+                printf("\nGoodbye!\n");
+            break;
+            default:
+                printf("\nChoose a valid option!\n");
+            break;
+        }
+
+    } while (choice != 4);
+}
+
+void menu(){
+    printf("\n======== MENU =========\n");
+    printf("1. Greeting\n");
+    printf("2. Even or Odd Number\n");
+    printf("3. Celsius to Fahrenheit\n");
+    printf("4. Exit Program\nChoice: ");
+}
